a5bf8f79dc14b784f344801da0952ad03a0a2253
kazim <kazim.guevenc@gmail.com>
2019-07-18 16:05:38 +0200

    HTML Seiten durchklickbar

--- src/main/java/project/post_ident/WebsiteController.java
+++ src/main/java/project/post_ident/WebsiteController.java
@@ -7,24 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebsiteController {
 
-    @GetMapping(value = "/zeigeCheckout")
-    public String zeigeCheckout(){
-        return "checkout";
-    }
-
-    @GetMapping(value = "/vergleich")
-    public String personenDatenAbgleichen(Model model) {
-        return "vergleich";
-    }
-
-
-
-/*
-    // Verbindung zur Bild- Datenbank
-    @Autowired
-    private BildRepository bildRepository;
-
- */
     // Startseite öffnen
     @GetMapping(value = "/")
     public String startSeiteOeffnen() {
@@ -34,10 +16,31 @@ public class WebsiteController {
     // Bild hochladen Methode
     @RequestMapping(value = "bildhochladen")
     public String bildHochladen() {
-
         // In die Methodenparameter einfügen?
-       // bildRepository.save(bild);
+        // bildRepository.save(bild);
         return "vergleich";
     }
+
+    @GetMapping(value = "/vergleich")
+    public String personenDatenAbgleichen(Model model) {
+        return "vergleich";
+    }
+
+    @GetMapping(value = "/datenAendern")
+    public String personenDatenAendern(Model model) {
+        return "datenAendern";
+    }
+
+
+    @GetMapping(value = "/zeigeCheckout")
+    public String zeigeCheckout(){
+        return "checkout";
+    }
+
+
+
+
+
+
 
 }
