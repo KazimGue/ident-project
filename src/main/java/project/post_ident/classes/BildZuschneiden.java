4c5e5b0677a42354f686352b1360a3224e3fd255
kazim <kazim.guevenc@gmail.com>
2019-07-23 17:12:07 +0200

    Bildgröße bis 10MB wird hochgeladen und Bild wid auch zu 1018x647 px verkleinert. Aber drehen und von jpg. in png umwandeln geschieht noch händig

--- src/main/java/project/post_ident/classes/BildZuschneiden.java
+++ src/main/java/project/post_ident/classes/BildZuschneiden.java
@@ -1,9 +1,12 @@
 package project.post_ident.classes;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.Buffer;
 
 
 //Klasse, die das hochgeladene Bild in mehrere Teile zugeschnitten wird,
@@ -12,41 +15,69 @@ public class BildZuschneiden {
 
         public static void bildZuschneiden() throws IOException {
 
-            try {
-            BufferedImage originalImage = ImageIO.read(new File("src\\main\\resources\\static\\images\\ausweisOlli.png"));
+        try {
+            BufferedImage originalImage = ImageIO.read(new File("src\\main\\resources\\static\\images\\PersoOlliGedreht.png"));
             //BufferedImage originalImage = ImageIO.read(new File("src\\main\\resources\\static\\images\\ausweisOlli.png"));
 
-                //width and height of each piece
-                int firstPartWidth = 298;
-                int firstPartHeight = 175;
+            // Resize originalImage to 1018x647 px um zu schneiden
+            BufferedImage resizedImage = resize(originalImage, 647 , 1018);
 
-                int firstX = 440;
-                int firstY = 75;
+            File output = new File("src\\main\\resources\\static\\images\\Personalausweis_resized-500x500.png");
+            ImageIO.write(resizedImage, "png", output);
 
-                int secondPartWidth = 490;
-                int secondPartHeight = 170;
-
-                int secondX =440;
-                int secondY = 235;
-
-                try {
-                    BufferedImage SubImgage = originalImage.getSubimage(firstX, firstY, firstPartWidth, firstPartHeight);
-                    File outputfileName = new File("src\\main\\resources\\static\\images\\personalausweisVorneName.png");
-                    ImageIO.write(SubImgage, "png", outputfileName);
-
-                    BufferedImage SubImgageTwo = originalImage.getSubimage(secondX, secondY, secondPartWidth, secondPartHeight);
-                    File outputfileGeburt = new File("src\\main\\resources\\static\\images\\personalausweisVorneGeburtstag.png");
-                    ImageIO.write(SubImgageTwo, "png", outputfileGeburt);
+            BufferedImage resized = ImageIO.read(new File("src\\main\\resources\\static\\images\\Personalausweis_resized-500x500.png"));
 
 
+//                BufferedImage resized = new BufferedImage();
+
+
+            //width and height of each piece
+            int firstPartWidth = 298;
+            int firstPartHeight = 175;
+
+            int firstX = 440;
+            int firstY = 75;
+
+            int secondPartWidth = 490;
+            int secondPartHeight = 170;
+
+            int secondX =440;
+            int secondY = 235;
+
+            try {
+                BufferedImage SubImgage = resized.getSubimage(firstX, firstY, firstPartWidth, firstPartHeight);
+                File outputfileName = new File("src\\main\\resources\\static\\images\\personalausweisVorneName.png");
+                ImageIO.write(SubImgage, "png", outputfileName);
+
+                BufferedImage SubImgageTwo = resized.getSubimage(secondX, secondY, secondPartWidth, secondPartHeight);
+                File outputfileGeburt = new File("src\\main\\resources\\static\\images\\personalausweisVorneGeburtstag.png");
+                ImageIO.write(SubImgageTwo, "png", outputfileGeburt);
 
             } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } catch (IOException e) {
                 e.printStackTrace();
             }
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+    }
+
+
+    private static BufferedImage resize(BufferedImage img, int height, int width) {
+        Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = resized.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+
+        return resized;
+    }
+
+
+
+
+
+
     }
 
